a8a0c4d2ef43ffb295475eec5fa7158c980371b7
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2019-05-24 01:35:18 +0530

    codet first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'shomikprogrammer@gmail.com',
+      pass: 'drishtijain08'
+    }
+  },
+  dbConnstring: 'mongodb://admin:<123>@cluster0-shard-00-00-r5ikp.mongodb.net:27017,cluster0-shard-00-01-r5ikp.mongodb.net:27017,cluster0-shard-00-02-r5ikp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  sessionKey: 'HaloCodet'
+}
